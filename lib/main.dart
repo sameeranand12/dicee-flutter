b37fe4c2bda8690804717df739543286af8d7abd
sameeranand12 <201951134@iiitvadodara.ac.in>
2021-05-25 06:56:57 +0530

    first

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Center(child: Text('Dicee')),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          //flex :1,
+    child:Image.asset('images/dice1.png'),),
+        Expanded(
+         // flex:2,
+        child:Image.asset('images/dice1.png'),),
+      ],
+    );
+  }
+}
