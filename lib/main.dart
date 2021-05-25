66d928da4b1cbefcb607da2e0b2703bad3926b27
sameeranand12 <201951134@iiitvadodara.ac.in>
2021-05-25 07:10:07 +0530

    padding done

--- lib/main.dart
+++ lib/main.dart
@@ -18,15 +18,24 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          //flex :1,
-    child:Image.asset('images/dice1.png'),),
-        Expanded(
-         // flex:2,
-        child:Image.asset('images/dice1.png'),),
-      ],
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            //flex :1,
+      child:Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Image.asset('images/dice1.png'),
+      ),),
+          SizedBox(width: 20.0,),
+          Expanded(
+           // flex:2,
+          child:Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.asset('images/dice1.png'),
+          ),),
+        ],
+      ),
     );
   }
 }
